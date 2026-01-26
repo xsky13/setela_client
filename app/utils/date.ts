9c517586c28f7590fa5c58b1eb3565620a180c80
Jared Peter <aljapeter05@gmail.com>
2026-01-26 17:37:49 -0300

    added function for converting date to input

--- app/utils/date.ts
+++ app/utils/date.ts
@@ -20,3 +20,15 @@ export const getMinutesDifference = (date1: string, date2: string): number => {
     const diffInMs = end - start;
     return Math.floor(diffInMs / (1000 * 60));
 };
+
+export const formatDateForInput = (utcString: string) => {
+    const date = new Date(utcString);
+    
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+};
