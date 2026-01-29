8a8b07bfc2fe1df00efe8acd39985371149d4289
Jared Peter <aljapeter05@gmail.com>
2026-01-29 16:20:26 -0300

    added filter for non visible items

--- app/routes/courses/course/course.tsx
+++ app/routes/courses/course/course.tsx
@@ -49,30 +49,12 @@ export default function Course() {
         const sortedItems = allItems.sort((a, b) => a.displayOrder - b.displayOrder);
         setCourseData(sortedItems);
 
+        // remove all none visible items
+        if (!course.currentUserIsOwner) {
+            setCourseData(prevData => prevData.filter(item => item.visible));
+        }
+
     }, [course]);
-
-    const formatDate = (dateString: string) => {
-        const date = new Date(dateString);
-
-        if (isNaN(date.getTime())) return "Fecha no disponible";
-        return date.toLocaleString('es-AR', {
-            timeZone: 'UTC',
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: false,
-            day: '2-digit',
-            month: '2-digit',
-            year: '2-digit'
-        });
-    };
-
-    const getMinutesDifference = (date1: string, date2: string): number => {
-        const start = new Date(date1).getTime();
-        const end = new Date(date2).getTime();
-
-        const diffInMs = end - start;
-        return Math.floor(diffInMs / (1000 * 60));
-    };
 
     return (
         <div>
